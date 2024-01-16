11b0706b157bde77de5a2c86df798be584cf597a
Bartosz Gawin <bartex.gw@gmail.com>
2024-01-16 11:56:56 +0000

    all dependendencies correctly resolved for GstWebRTC and GstSdp

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst
+gi.require_version('GstWebRTC', '1.0')
+from gi.repository import GstWebRTC
+gi.require_version('GstSdp', '1.0')
+from gi.repository import GstSdp
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -9,12 +13,13 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="/templates")
-app.mount("/static", StaticFiles(directory="/static"), name="static")
+templates = Jinja2Templates(directory="/home/ubuntu/src/templates")
+app.mount("/static", StaticFiles(directory="/home/ubuntu/src/static"), name="static")
 
 Gst.init(None)
-# pipeline = Gst.parse_launch("webrtcbin name=webrtcbin ! vp8dec ! videoconvert ! autovideosink")
 print("GStreamer version:", Gst.version())
+
+pipeline = Gst.parse_launch("webrtcbin name=webrtcbin ! vp8dec ! videoconvert ! autovideosink")
 
 
 @app.get("/")
