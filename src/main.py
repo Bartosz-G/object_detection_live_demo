01609db3b032883e784c409deea593a3c2e9c14c
Bartosz Gawin <bartex.gw@gmail.com>
2024-01-19 15:47:56 +0000

    SDP offer with codecs is being received by the server

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import gi
 gi.require_version('Gst', '1.0')
-from gi.repository import Gst
+from gi.repository import Gst, GLib
 gi.require_version('GstWebRTC', '1.0')
 from gi.repository import GstWebRTC
 gi.require_version('GstSdp', '1.0')
 from gi.repository import GstSdp
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+import json
 
 
 
@@ -16,37 +18,67 @@ app = FastAPI()
 templates = Jinja2Templates(directory="/home/ubuntu/src/templates")
 app.mount("/static", StaticFiles(directory="/home/ubuntu/src/static"), name="static")
 
-def on_offer_created(promise, _, webrtcbin):
-    promise = promise.wait()
-    reply = promise.get_reply()
-    offer = reply.get_value('offer')
-
-    promise = Gst.Promise.new()
-    webrtcbin.emit('set-local-description', offer, promise)
-    promise.interrupt()
-
-    # Convert the offer to a string and send it to the client
-    sdp_str = offer.sdp.as_text()
-    print("SDP Offer:\n{}".format(sdp_str))
-    # Here you would typically send the SDP offer to the client via your signaling mechanism
-
-def on_negotiation_needed(element, _):
-    promise = Gst.Promise.new_with_change_func(on_offer_created, element, element)
-    element.emit('create-offer', None, promise)
 
 Gst.init(None)
-print("GStreamer version:", Gst.version())
 
 pipeline = Gst.parse_launch(
     "webrtcbin name=webrtcbin ! decodebin name=decoder ! queue name=queue ! videoscale name=videoscale ! "
     "capsfilter name=capsfilter ! videoconvert name=convert"
 )
 webrtcbin = pipeline.get_by_name("webrtcbin")
-webrtcbin.connect("on-negotiation-needed", on_negotiation_needed)
-webrtcbin.emit('create-offer', None, None)
+
+res, sdpmsg = GstSdp.SDPMessage.new()
+print(f'res: {res}')
+print(f'sdpmsg: {sdpmsg.as_text()}')
+
+
+class Offer(BaseModel):
+    type: str
+    offer: dict
 
 @app.get("/")
 async def get(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+@app.post("/offer")
+async def receive_offer(offer: Offer):
+    print("Received offer:", offer.json())
+    return {"status": 200}
 
 
+
+
+
+
+
+
+
+
+
+# def on_offer_created(promise, _, webrtcbin):
+#     promise = promise.wait()
+#     reply = promise.get_reply()
+#     offer = reply.get_value('offer')
+#
+#     promise = Gst.Promise.new()
+#     webrtcbin.emit('set-local-description', offer, promise)
+#     promise.interrupt()
+#
+#     # Convert the offer to a string and send it to the client
+#     sdp_str = offer.sdp.as_text()
+#     print("SDP Offer:\n{}".format(sdp_str))
+#     # Here you would typically send the SDP offer to the client via your signaling mechanism
+#
+# def on_negotiation_needed(element, _):
+#     promise = Gst.Promise.new_with_change_func(on_offer_created, element, element)
+#     element.emit('create-offer', None, promise)
+#
+#
+#
+#
+# pipeline.set_state(Gst.State.PLAYING)
+# print("Pipeline started")
+# loop = GLib.MainLoop()
+# try:
+#     loop.run()
+# except KeyboardInterrupt:
+#     loop.quit()
--- src/static/main.js
+++ src/static/main.js
@@ -1,11 +1,63 @@
+const iceServers = [
+    {
+        urls: ['stun:stun.kinesisvideo.eu-west-2.amazonaws.com:443', 'stun.l.google.com']
+    }
+];
+
+const ip = '13.42.36.213';
+const offerEndpoint = '/offer';
+
+let localStream;
+let remoteStream;
+let peerConnection;
 
 let init = async () => {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        console.error('MediaDevices API not supported or blocked in this browser.');
+        return;
+    }
     try {
-        let localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+        localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
         document.getElementById('viewer').srcObject = localStream;
     } catch (error) {
         console.error('Error accessing media devices.', error);
     }
+
+    createOffer()
 };
+
+let fetchOffer = async (serverEndpoint, message) => {
+    fetch(serverEndpoint, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: message})
+        .then(response => response.json())
+        .then(data => {console.log('Success:', data);})
+        .catch((error) => {console.error('Error:', error);});
+
+}
+
+let createOffer = async () => {
+    peerConnection = new RTCPeerConnection( iceServers );
+
+    localStream.getTracks().forEach((track) => {
+            peerConnection.addTrack(track, localStream)
+        });
+
+    let offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer);
+
+
+    let message = JSON.stringify({'type': 'offer', 'offer': offer})
+
+    console.log("Local Offer:", message);
+
+    fetchOffer(offerEndpoint, message);
+
+
+}
+
 
 init();
