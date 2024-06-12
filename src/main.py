d79142e09f6900b4d5cb2c88c42640ea654748bc
Bartosz Gawin <bartex.gw@gmail.com>
2024-06-12 18:14:12 +0100

    added logging to webrtcclient

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException, WebSocket, BackgroundTasks
-from starlette.concurrency import run_in_threadpool
+from fastapi import FastAPI, Request, HTTPException, WebSocket
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
@@ -12,21 +11,19 @@ gi.require_version('GstSdp', '1.0')
 from gi.repository import GstSdp
 
 from contextlib import asynccontextmanager
-from multiprocessing import shared_memory
 import torch
 import torch_neuron
 from torchvision.ops import box_convert
 import numpy as np
-import multiprocessing
 import threading
 import queue
 import asyncio
 import json
 import os
 import sys
+import logging
 
-# For measuring performance
-from functools import wraps
+
 from time import perf_counter, sleep
 
 from services.webrtcclient import WebRTCClient
@@ -43,215 +40,11 @@ TOPK = 10
 
 
 
-websocket_connection = None
-
-
-def timing(f):
-    @wraps(f)
-    def wrap(*args, **kw):
-        ts = perf_counter()
-        result = f(*args, **kw)
-        te = perf_counter()
-        elapsed_time = (te - ts) * 1000
-        print(f"Function '{f.__name__}' executed in {elapsed_time:.2f} ms")
-        return result
-    return wrap
-
-
-
-
-# def pull_samples(appsink, connection, lock, process):
-#     # global pipeline
-#     print('--- pipe_samples initiated ---')
-#
-#
-#     try:
-#         while True:
-#             sample = appsink.emit("pull-sample") # Dereferencing a pointer
-#             if sample is None:
-#                 sleep(1)
-#                 print(f'No samples in the appsink')
-#                 continue
-#
-#             caps = sample.get_caps()
-#             print(f'[pull_samples] APPSINK CAPABILITIES: {caps.to_string()}')
-#
-#             assert HEIGHT == caps.get_structure(0).get_value("height"), f'[pull_samples] appsink receive height: {caps.get_structure(0).get_value("height")}, expected: {HEIGHT}'
-#             assert WIDTH == caps.get_structure(0).get_value("width"), f'[pull_samples] appsink receive width: {caps.get_structure(0).get_value("width")}, expected: {WIDTH}'
-#
-#             buffer = sample.get_buffer()
-#             success, map_info = buffer.map(Gst.MapFlags.READ)
-#
-#             if not success:
-#                 raise RuntimeError("Could not map buffer data!")
-#
-#             warmup_frame = np.frombuffer(map_info.data, dtype=np.uint8).reshape((HEIGHT, WIDTH, 3)) / 255
-#
-#             # warmup_frame = np.ndarray(
-#             #     shape=(HEIGHT, WIDTH, 3),
-#             #     dtype=np.uint8,
-#             #     buffer=map_info.data) / 255
-#
-#             warmup_frame = np.transpose(warmup_frame, (2, 0, 1))
-#             buffer.unmap(map_info)
-#
-#             frame_nbytes, frame_dtype, frame_shape = warmup_frame.nbytes, warmup_frame.dtype, warmup_frame.shape
-#
-#
-#             memory = shared_memory.SharedMemory(create=True, size=frame_nbytes)
-#
-#             with lock:
-#                 send_array = np.ndarray(frame_shape, dtype=frame_dtype, buffer=memory.buf)
-#                 np.copyto(send_array, warmup_frame)
-#
-#             init_message = {
-#                 'state': 'init',
-#                 'shared_memory_name': memory.name,
-#                 'dtype': frame_dtype,
-#                 'shape': frame_shape,
-#                 'nbytes': frame_nbytes
-#             }
-#
-#             connection.send(init_message)
-#             break
-#
-#         while process.is_alive():
-#             sample = appsink.emit("pull-sample") # Dereferencing a pointer
-#             if sample is None:
-#                 continue
-#
-#
-#             buffer = sample.get_buffer()
-#             _, map_info = buffer.map(Gst.MapFlags.READ)
-#
-#             frame = np.frombuffer(map_info.data, dtype=np.uint8).reshape((HEIGHT, WIDTH, 3)) / 255
-#             buffer.unmap(map_info)
-#
-#             # frame = np.ndarray(
-#             #     shape=(HEIGHT, WIDTH, 3),
-#             #     dtype=np.uint8,
-#             #     buffer=map_info.data) / 255
-#
-#             frame = np.transpose(frame, (2, 0, 1))
-#             print(f'[pull_samples] sending input: {frame[0, 0, :10]}')
-#             with lock:
-#                 shared_array = np.ndarray(frame_shape, dtype=frame_dtype, buffer=memory.buf)
-#                 np.copyto(shared_array, frame)
-#
-#
-#             connection.send({'state': 'cls'})
-#
-#     except Exception as e:
-#         print(f'Exception in pull_samples occured: {e}')
-
-
-# def model_process(model_path, receive_conn, post_conn, receive_lock, send_lock):
-#
-#     model = torch.jit.load(model_path)
-#
-#     try:
-#         while True:
-#             msg = receive_conn.recv()
-#
-#             if msg['state'] not in ('cls', 'init', 'close'):
-#                 raise Exception(f'received an unrecognised message from receive connection: {msg}')
-#
-#             if msg['state'] == 'log':
-#                 post_conn.send(msg)
-#                 continue
-#
-#             if msg['state'] == 'init':
-#                 post_conn.send({'state': 'log', 'log': 'starting_warmup_process'})
-#                 receive_shm_name, dtype, shape = msg['shared_memory_name'], msg['dtype'], msg['shape']
-#                 receive_shared_memory = multiprocessing.shared_memory.SharedMemory(name=receive_shm_name)
-#
-#                 post_conn.send({'state': 'log', 'log': 'pulling_warmup_frame'})
-#                 with receive_lock:
-#                     warmup_frame = np.ndarray(shape, dtype=dtype, buffer=receive_shared_memory.buf)
-#
-#                 post_conn.send({'state':'log', 'log': 'warming-up model'})
-#                 warmup_frame = torch.from_numpy(warmup_frame).unsqueeze(0).float()
-#                 output = model(warmup_frame)
-#                 post_conn.send({'state': 'log', 'log': 'warming-up complete'})
-#
-#                 logits, bboxes = output[0].numpy(), output[1].numpy()
-#                 logits_nbytes, logits_dtype, logits_shape = logits.nbytes, logits.dtype, logits.shape
-#                 bboxes_nbytes, bboxes_dtype, bboxes_shape = bboxes.nbytes, bboxes.dtype, bboxes.shape
-#
-#                 post_conn.send({'state':'log', 'log': 'creating shared memory for the postprocessor'})
-#                 send_logits_shared_memory = multiprocessing.shared_memory.SharedMemory(create=True, size=logits_nbytes)
-#                 send_bboxes_shared_memory = multiprocessing.shared_memory.SharedMemory(create=True, size=bboxes_nbytes)
-#
-#                 post_conn.send({'state': 'log', 'log': 'putting warmup logits and frames to memory'})
-#                 with send_lock:
-#                     shared_logits = np.ndarray(logits_shape, dtype=logits_dtype, buffer=send_logits_shared_memory.buf)
-#                     np.copyto(shared_logits, logits)
-#                     shared_bboxes = np.ndarray(bboxes_shape, dtype=bboxes.dtype, buffer=send_bboxes_shared_memory.buf)
-#                     np.copyto(shared_bboxes, bboxes)
-#
-#                 init_message = {
-#                     'state': 'init',
-#                     'logits': {
-#                         'shared_memory_name': send_logits_shared_memory.name,
-#                         'dtype': logits_dtype,
-#                         'shape': logits_shape,
-#                         'nbytes': logits_nbytes
-#                     },
-#                     'bboxes': {
-#                         'shared_memory_name': send_bboxes_shared_memory.name,
-#                         'dtype': bboxes_dtype,
-#                         'shape': bboxes_shape,
-#                         'nbytes': bboxes_nbytes
-#                     }
-#                 }
-#
-#                 post_conn.send(init_message)
-#
-#                 continue
-#
-#             #TODO: Refactor to not have to wait for a message and be able to respond to messages
-#             if msg['state'] == 'cls':
-#                 with receive_lock:
-#                     received_array = np.ndarray(shape, dtype=dtype, buffer=receive_shared_memory.buf)
-#                     # print(f'[model_process] taken input: {received_array[0, 0, :10]}')
-#                 # post_conn.send({'state': 'log', 'log': {'received_array': received_array[:4, :4]}})
-#                 frame = torch.from_numpy(received_array).float().unsqueeze(0)
-#                 print(f'[model_process] received input: {frame[0, 0, 0, :10]}')
-#
-#
-#                 ts = perf_counter()
-#                 # post_conn.send({'state': 'log', 'log': {'frame ': frame[:4, :4]}})
-#                 # post_conn.send({'state': 'log', 'log': {'frame_shape ': frame.shape}})
-#                 output = model(frame)
-#                 te = perf_counter()
-#                 elapsed_time = (te - ts) * 1000
-#                 latency = elapsed_time
-#
-#                 # Unpacking bboxes and labels
-#                 logits, bbox_pred = output[0].numpy(), output[1].numpy()
-#                 # post_conn.send({'state': 'log', 'log':{'logits': logits[:4], 'bboxes': bbox_pred[:4]}})
-#                 # ============= Still correct =======================
-#
-#                 with send_lock:
-#                     shared_logits = np.ndarray(logits_shape, dtype=logits_dtype, buffer=send_logits_shared_memory.buf)
-#                     np.copyto(shared_logits, logits)
-#                     shared_bboxes = np.ndarray(bboxes_shape, dtype=bboxes.dtype, buffer=send_bboxes_shared_memory.buf)
-#                     np.copyto(shared_bboxes, bboxes)
-#
-#                 post_conn.send({'state': 'cls', 'latency': latency})
-#
-#     except Exception as e:
-#         post_conn.send(e)
-
 
 class PostProcessor:
     def __init__(self, topk):
-        # self.lock = lock
         self.topk = topk
 
-    # def set_topk(self, topk):
-    #     with self.lock:
-    #         self.topk = topk
 
     def __call__(self, logits, boxes, *args, **kwargs):
         # with self.lock:
@@ -273,56 +66,6 @@ class PostProcessor:
 
 
 
-# #TODO: Impement configurable post_processor
-# def prediction_listener(connection, process, lock, postprocessor, webrtc):
-#     print('--- pipe_listener initiated ---')
-#
-#     while process.is_alive():
-#         message = connection.recv()
-#         if message['state'] == 'log':
-#             print(f"[model]: {message['log']}")
-#             continue
-#
-#         if message['state'] == 'init':
-#             print(f"[prediction_listener]: received init from the model")
-#             init_logits = message['logits']
-#             init_bboxes = message['bboxes']
-#
-#             logits_shm_name, logits_nbytes, logits_dtype, logits_shape = (
-#                 init_logits['shared_memory_name'], init_logits['nbytes'], init_logits['dtype'], init_logits['shape'])
-#             bboxes_shm_name, bboxes_nbytes, bboxes_dtype, bboxes_shape = (
-#                 init_bboxes['shared_memory_name'], init_bboxes['nbytes'], init_bboxes['dtype'], init_bboxes['shape'])
-#
-#             logits_shared_memory = multiprocessing.shared_memory.SharedMemory(name=logits_shm_name)
-#             bboxes_shared_memory = multiprocessing.shared_memory.SharedMemory(name=bboxes_shm_name)
-#
-#             print(f"[prediction_listener]: trying to access logits and bboxes")
-#             with lock:
-#                 logits = np.ndarray(logits_shape, dtype=logits_dtype, buffer=logits_shared_memory.buf)
-#                 bboxes = np.ndarray(bboxes_shape, dtype=bboxes_dtype, buffer=bboxes_shared_memory.buf)
-#
-#             print(f"[prediction_listener], logits: {logits.shape}, bboxes: {bboxes.shape}")
-#             print(f'[prediction_listener], init successful!')
-#             continue
-#
-#         if message['state'] == 'cls':
-#             latency = message['latency']
-#             print(f"[model] latency: {latency}")
-#             with lock:
-#                 logits = np.ndarray(logits_shape, dtype=logits_dtype, buffer=logits_shared_memory.buf)
-#                 bboxes = np.ndarray(bboxes_shape, dtype=bboxes_dtype, buffer=bboxes_shared_memory.buf)
-#
-#             # print(f'[prediction_listener] bboxes: {bboxes[:4]}')
-#             labels, bboxes = postprocessor(logits, bboxes)
-#             # print(f"[prediction_listener], post_logits_shape: {labels.shape}, post_bboxes_shape: {bboxes.shape}")
-#             # print(f"[prediction_listener], post_logits_dtype: {labels.dtype}, post_bboxes_dtype: {bboxes.dtype}")
-#             predictions_bytearray = encode_predictions(latency=latency, labels=labels, bboxes=bboxes)
-#             glib_bytes = GLib.Bytes.new(predictions_bytearray)
-#
-#             # print(f'[prediction_listener]: sending bytearrays!')
-#             webrtc.send_data_channel_message(glib_bytes)
-#
-#             continue
 
 def encode_predictions(latency, labels, bboxes):
     latency = np.uint8(latency)
@@ -352,9 +95,23 @@ def encode_predictions(latency, labels, bboxes):
 async def lifespan(app: FastAPI):
     global webrtc
     global prediction_task
+    global logger
+    logger = logging.getLogger("uvicorn")
+    def log_assertions(exc_type, exc_value, exc_traceback):
+        if issubclass(exc_type, AssertionError):
+            logger.critical("", exc_info=(exc_type, exc_value, exc_traceback))
+        else:
+            sys.__excepthook__(exc_type, exc_value, exc_traceback)
+
+    sys.excepthook = log_assertions
+
+
+
     Gst.init(None)
+
     webrtc = WebRTCClient(1, 1, {})
     prediction_task = PredictionTask(config={})
+
 
     yield
 
@@ -363,8 +120,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-# templates = Jinja2Templates(directory="templates")
-# app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 templates = Jinja2Templates(directory="/home/ubuntu/src/templates")
 app.mount("/static", StaticFiles(directory="/home/ubuntu/src/static"), name="static")
--- src/services/webrtcclient.py
+++ src/services/webrtcclient.py
@@ -12,6 +12,7 @@ import json
 
 class WebRTCClient:
     def __init__(self, id_: int, peer_id: str | int, config: dict):
+        global logger
         self.id_ = id_
         self.peer_id = peer_id
         self.websocket = None
@@ -20,6 +21,7 @@ class WebRTCClient:
         self.appsink = None
         self.datachannel = None
         self.bus = None
+        self.logger = logger
 
         self.height = config.get('height', 480)
         self.width = config.get('width', 480)
@@ -44,6 +46,7 @@ class WebRTCClient:
         self.pipeline = Gst.Pipeline.new("pipeline")
         self.pipeline.add(self.webrtcbin)
 
+        self.logger.debug(f'[WebRTCClient]: initiated')
 
     def get_webrtcbin(self):
         return self.webrtcbin
@@ -59,40 +62,43 @@ class WebRTCClient:
 
     def on_negotiation_needed(self, *args):
         assert self.websocket, "on_negotiation was called before websocket_connection was created"
-        print("ON_NEGOTIATION_NEEDED was called: ", args)
+        self.logger.debug(f'[WebRTCClient]: on_negotiation_needed called')
         _ = asyncio.run(self.websocket.send_text('waitingForOffer'))
 
     def send_ice_candidate_message(self, _, mlineindex, candidate):
-        print(f'INITIATING SENDING OF ICE CANDIDATE \n')
+        self.logger.debug(f'[WebRTCClient]: send_ice_candidate_message called')
         assert self.websocket, f"send_ice_candidate_message was called before websocket_connection was created! \n candidate: {candidate} \n mlineindex: {mlineindex}"
         icemsg = json.dumps({'ice': {'candidate': candidate, 'sdpMLineIndex': mlineindex}})
 
-        print(f'ICE candidate being sent: {icemsg}')
+        self.logger.debug(f'[WebRTCClient] - send_ice_candidate_message: sending over ICE candidate: \n {icemsg}')
         asyncio.run(self.websocket.send_text(icemsg))
 
     def on_answer_created(self, promise, _):
-        print(f'On answer creation initiated... \n')
+        self.logger.debug(f'[WebRTCClient] - on_answer_created: on_answer_created called')
         promise.wait()
         msg = promise.get_reply()
         answer = msg.get_value('answer')
-        print(f'ANSWER: \n {answer.sdp.as_text()}')
+        self.logger.info(f'[WebRTCClient] - on_answer_created: sdp answer received')
+        self.logger.debug(f'[WebRTCClient] - on_answer_created: ANSWER \n {answer.sdp.as_text()}')
         self.webrtcbin.emit('set-local-description', answer, promise)
         promise.interrupt()
-        print(f'ON ANSWER CREATION COMPLETE \n')
+        self.logger.debug(f'[WebRTCClient] - on_answer_created: local description set')
 
         reply = json.dumps({'sdp': {'type': 'answer', 'sdp': answer.sdp.as_text()}})
 
+        self.logger.info(f'[WebRTCClient] - on_answer_created: sending over reply \n {reply}')
+        self.logger.debug(f'[WebRTCClient] - on_answer_created: REPLY \n {reply}')
         asyncio.run(self.websocket.send_text(reply))
 
     def on_decodebin_added(self, _, pad):
-        print(f'[on_decodebin_added]: CALLED')
+        self.logger.debug(f'[WebRTCClient]: on_decodebin_added called')
 
         if not pad.has_current_caps():
-            print(pad, 'has no caps, ignoring')
+            self.logger.error(f'[WebRTCClient] - on_decodebin_added: pad has no caps, ignoring {pad}')
             return
 
         caps = pad.get_current_caps()
-        print(f'[on_decodebin_added]: INCOMING STREAM CAPABILITIES: {caps.to_string()}')
+        self.logger.debug(f'[WebRTCClient] - on_decodebin_added: incoming stream capabilities \n {caps.to_string()}')
         name = caps.to_string()
 
         q = Gst.ElementFactory.make('queue')
@@ -114,24 +120,25 @@ class WebRTCClient:
         scale.link(conv)
         conv.link(capsfilter)
         capsfilter.link(self.appsink)
+        #TODO: Log all of the elements of the pipeline
 
+        self.logger.debug(f'[WebRTCClient] - on_decodebin_added: setting pipeline to playing')
         self.pipeline.set_state(Gst.State.PLAYING)
 
     def on_stream_added(self, _, pad):
-        print(f'------ ON STREAM ADDED EVENT HAS BEEN CALLED ---------')
-        print(f'------ STREAM IS NOW FLOWING  ---------')
+        self.logger.debug(f'[WebRTCClient]: on_stream_added called')
+        self.logger.debug(f'[WebRTCClient] - on_stream_added: stream recognised attaching pipeline')
 
         decodebin = Gst.ElementFactory.make('decodebin')
         decodebin.connect('pad-added', self.on_decodebin_added)
         self.pipeline.add(decodebin)
         decodebin.sync_state_with_parent()
         self.webrtcbin.link(decodebin)
+        self.logger.info(f'[WebRTCClient] - on_stream_added: stream now flowing')
 
-        print(f'----- Everything connected, stream should be flowing')
 
-#TODO: Fix the Datachannel
     def on_data_channel_ready(self, channel):
-        print("[DataChannel]: ready")
+        self.logger.info(f'[WebRTCClient] - on_data_channel_ready: data channel ready, attaching')
         self.datachannel = channel
         # send_data_channel_message(b"Hello, Data Channel!")
 
@@ -143,12 +150,12 @@ class WebRTCClient:
     #     print("[DataChannel], message received:", data)
 
     def send_data_channel_message(self, data):
-        assert self.datachannel, f'[send_data_channel_message]: called but self.data_channels is {self.datachannel}'
+        assert self.datachannel, f'[WebRTCClient] - send_data_channel_message: called but self.data_channels is {self.datachannel}'
         # print(f"[DataChannel], dir: {dir(data_channel)}")
         self.datachannel.send_data(data)
 
     def on_data_channel(self, _, data_channel):
-        print("[DataChannel], created")
+        self.logger.debug(f"[WebRTCClient] - on_data_channel: received")
         # data_channel.connect("notify::ready-state", self.on_data_channel_open)
         data_channel.connect("on-open", self.on_data_channel_ready)
 
@@ -156,15 +163,17 @@ class WebRTCClient:
     def receive_message(self, msg):
         if 'offer' in msg:
             sdp = msg['offer']['sdp']
-            print(f'Received an offer: \n {sdp}')
+            self.logger.info(f"[WebRTCClient] - receive_message: received an offer")
+            self.logger.debug(f"[WebRTCClient] - receive_message: OFFER \n {sdp}")
             res, sdpmsg = GstSdp.SDPMessage.new()
             GstSdp.sdp_message_parse_buffer(bytes(sdp.encode()), sdpmsg)
             offer = GstWebRTC.WebRTCSessionDescription.new(GstWebRTC.WebRTCSDPType.OFFER, sdpmsg)
             promise = Gst.Promise.new()
             self.webrtcbin.emit('set-remote-description', offer, promise)
             promise.interrupt()
+            self.logger.debug(f"[WebRTCClient] - receive_message: remote description set")
 
-            print('Creating an answer ... \n')
+            self.logger.debug(f"[WebRTCClient] - receive_message: creating an answer")
             promise = Gst.Promise.new_with_change_func(self.on_answer_created, None)
             self.webrtcbin.emit('create-answer', None, promise)
 
@@ -172,7 +181,8 @@ class WebRTCClient:
 
         elif 'ice' in msg:
             ice = msg['ice']
-            print(f'Received an ICE candidate: {ice}')
+            self.logger.info(f'[WebRTCClient] - receive_message: received an ICE candidate')
+            self.logger.debug(f'[WebRTCClient] - receive_message: ICE \n {ice}')
             candidate = ice['candidate']
             sdpmlineindex = ice['sdpMLineIndex']
             self.webrtcbin.emit('add-ice-candidate', sdpmlineindex, candidate)
